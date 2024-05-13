a747f403e189dfa60498dab04cb195ead936fb75
Sandra Davies <dav.sandra1970@gmail.com>
2024-05-13 13:07:47 +0200

    Booklist

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
+<script>
+import HeaderLayout from "./components/HeaderLayout.vue";
+import Booklist from "./components/Booklist.vue";
+export default {
+  name: "app",
+  components: {
+    HeaderLayout,
+    Booklist,
+  },
+};
+</script>
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HeaderLayout />
+  <Booklist />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+:root {
+  --primary: #6a1cc3;
+  --primary-dark: #370b68;
+  --secondary: #f39c12;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+h1,
+h2,
+h3,
+h4 {
+  color: var(--primary);
+  margin-bottom: 15px;
+}
+
+.inside {
+  margin: 10px;
+}
+
+@media only screen and (min-width: 1140px) {
+  .inside {
+    width: 960px;
+    margin: 0 auto;
+  }
 }
 </style>
